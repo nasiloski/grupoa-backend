58794ceb6c3862ff2732027bb18ac196b8517a0a
Daniel Nasiloski <danielnasiloski@MacBook-Pro-de-Daniel.local>
2020-05-21 17:04:35 -0300

    Inclusão da função de auto popular um banco de dados configurado. Inclui automaticamente o usuario admin para gerenciamento dos alunos

--- src/services/database.js
+++ src/services/database.js
@@ -1,8 +1,46 @@
 const mysql = require("mysql");
 const mysqlConfig = require("../config/mysql");
-
 const connection = mysql.createConnection(mysqlConfig);
 connection.connect();
+
+function createTables(){
+    const sqlAlunos = "create table if not exists alunos( \n" +
+                        "ra int(6) NOT NULL, \n" +
+                        "nome varchar (60) NOT NULL, \n" +
+                        "email varchar(50),\n" +
+                        "cpf int(11) NOT NULL, \n" +
+                        "primary key (ra) \n" +
+                        ");";
+
+    const sqlAdm = "create table if not exists admin_user( \n" +
+                    "cd_usuario int(4) auto_increment, \n" +
+                    "nome varchar(50) not null, \n" +
+                    "email varchar(50), \n" +
+                    "password varchar(50), \n" +
+                    "primary key (cd_usuario) \n" +
+                    ");"
+
+    connection.query(sqlAlunos, function(error, result, fields){
+        if(error) return console.log(error);
+        console.log("Tabela de alunos criada com sucesso ou ja existente ");
+    });
+    connection.query(sqlAdm, function(error, result, fields){
+        if(error) return console.log(error);
+        console.log("Tabela de adms criada com sucesso ou ja existente");
+    });
+    connection.query('select * from admin_user where nome="admin"', function(error, result, fields){
+        if(error) return console.log(error);
+        const data = result[0];
+        if(!data){
+            connection.query(`insert into admin_user(cd_usuario, nome, email, password) values (null, 'admin', 'admin@localhost', 'admin')`, function(error, result, fields){
+                if(error) return console.log(error);
+                console.log("Usuário ADM Criado");
+            });
+        }
+        
+    });
+}
+createTables();
 
 module.exports = {
     create (nome, email, ra, cpf) {
