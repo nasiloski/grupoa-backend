3d86f5e00e8d4ac26817aa9b46aa3ba3c81906a7
Daniel Nasiloski <danielnasiloski@MacBook-Pro-de-Daniel.local>
2020-06-10 18:09:21 -0300

    Adicionado frontend

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -30,21 +30,6 @@ module.exports = {
           DATABASE: 'grupoa_dev',
           TOKENPASSWD: 'Teste12345@!'
         }
-      },
-      {
-        name      : "Frontend",
-        script    : "npx",
-        interpreter: "none",
-        args: "serve -s ../grupoa_frontend/build -p 80",
-        env: {
-          PORT: '80'
-        },
-        env_production: {
-          PORT: '80'
-        },
-        env_homolog : {
-          PORT: '80'
-        }
-      },      
+      },     
     ]
   };
