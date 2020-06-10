27c7159e15906abbeecebe90ada03cfde895d65e
Daniel Nasiloski <danielnasiloski@MacBook-Pro-de-Daniel.local>
2020-06-10 15:19:38 -0300

    Criado o arquivo ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+    apps : [
+      {
+        name: 'API',
+        script: 'src/app.js',
+        cwd: __dirname ,
+        instances: 1,
+        autorestart: true,
+        wait_ready: true,
+        exp_backoff_restart_delay: 100,
+        max_memory_restart: '512M',
+        env: {
+          NDBHOST: '127.0.0.1',
+          DBUSER: 'grupoa',
+          DBPASSWD: 'grupoa',
+          DATABASE: 'grupoa',
+          TOKENPASSWD: 'Teste12345@!'
+        },
+        env_production: {
+          DBHOST: '127.0.0.1',
+          DBUSER: 'grupoa',
+          DBPASSWD: 'grupoa',
+          DATABASE: 'grupoa',
+          TOKENPASSWD: 'Teste12345@!'
+        },
+        env_homolog : {
+          DBHOST: '127.0.0.1',
+          DBUSER: 'grupoa',
+          DBPASSWD: 'grupoa',
+          DATABASE: 'grupoa',
+          TOKENPASSWD: 'Teste12345@!'
+        }
+      },   
+    ]
+  };
